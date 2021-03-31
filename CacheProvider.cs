03ae21a7bf1ff63d36ad0081066445593ebb0cb3
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2021-03-31 10:46:06 +0100

    Major: initial commit

---
+++ CacheProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace StockportGovUK.NetStandard.Abstractions.Caching
+{
+    public class CacheProvider
+    {
+        private readonly bool _allowCaching;
+
+        private readonly double _timeout;
+
+        private readonly IDistributedCache _cacheProvider;
+
+        public CacheProvider(IDistributedCache cacheProvider, bool allowCaching, double timeout = 20)
+        {
+            _allowCaching = allowCaching;
+            _timeout = timeout;
+            _cacheProvider = cacheProvider;
+        }
+
+        public async Task<string> GetStringAsync(string key)
+        {
+            if (_allowCaching)
+                return await _cacheProvider.GetStringAsync(key);
+
+            return null;
+        }
+
+        public async Task SetStringAsync(string key, string value)
+        {
+            if (_allowCaching)
+            {
+                await _cacheProvider.SetStringAsync(key, value, new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(_timeout)
+                });
+            }
+        }
+
+        public async Task SetStringAsync(string key, string value, DistributedCacheEntryOptions options)
+        {
+            if (_allowCaching)
+                await _cacheProvider.SetStringAsync(key, value, options);
+        }
+    }
+}
